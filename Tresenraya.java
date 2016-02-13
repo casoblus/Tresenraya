c784622be7183156819457450e75a269b4ee4ae2
casoblus <casobrinolusquinos@gmail.com>
2016-02-13 17:34:48 +0100

    pruebas

--- Tresenraya.java
+++ Tresenraya.java
@@ -15,11 +15,70 @@ public class Tresenraya
 			return 1;
 		}
 		// CHECK_WINS
-		private static boolean CHECK_WINS()
+		private static boolean CHECK_WINS( int player, int[][] tab )
 		{
+			// Empiezo por la casilla del centro
+			if ( tab[1][1] == player ) // La pieza central pertenece al jugador
+			{
+				if ( tab[0][1] == player && tab[2][1] == player ) 
+				{
+					System.out.println( "Jugador "+player+" gana." );
+					return true;
+				} 
+				else if ( tab[1][0] == player && tab[1][2] == player )
+				{
+					System.out.println( "Jugador "+player+" gana." );
+					return true;
+				}
+				else if ( tab[0][0] == player && tab[2][2] == player )
+				{
+					System.out.println( "Jugador "+player+" gana." );
+					return true;
+				}
+				else if ( tab[0][2] == player && tab[2][0] == player )
+				{
+					System.out.println( "Jugador "+player+" gana." );
+					return true;
+				}
+				
+					
+			} else {
+				// Solo quedan cuatro posibilidades
+				if ( tab[0][1] == player ) 
+				{
+					if ( tab[0][0] == player && tab[0][2] == player )
+					{
+							System.out.println( "Jugador "+player+" gana." );
+							return true;
+					}
+				} 
+				else if ( tab[1][0] == player )
+				{
+					if ( tab[0][0] == player && tab[2][0] == player )
+					{
+							System.out.println( "Jugador "+player+" gana." );
+							return true;
+					}
+				}
+				else if ( tab[2][1] == player )
+				{
+						if ( tab[2][0] == player && tab[2][2] == player )
+					{
+							System.out.println( "Jugador "+player+" gana." );
+							return true;
+					}
+				}
+				else if ( tab[1][2] == player )
+				{
+					if ( tab[0][2] == player && tab[2][2] == player )
+					{
+							System.out.println( "Jugador "+player+" gana." );
+							return true;
+					}
+				}
+			}
 			return false;
 		}
-		// REMOVE_FICHA
 		// PUT_FICHA
 		private static void PUT_FICHA( int player, int[][] tab )
 		{
@@ -64,13 +123,17 @@ public class Tresenraya
 				// Elimina la ficha seleccionada
 
 				System.out.println( "Introduzca ficha a mover: " );
-				do { 
-					System.out.println( "fila: " );
-					remFila = sc.nextInt();
-					System.out.println( "columna: " );
-					remColumna = sc.nextInt();
-					// Se repite mientras que la casilla no 
-					// contenga una ficha perteneciente al jugador
+				do {
+					do {
+						do {	
+							System.out.println( "fila: " );
+							remFila = sc.nextInt();
+							System.out.println( "columna: " );
+							remColumna = sc.nextInt();
+							// Se repite mientras que la casilla no 
+							// contenga una ficha perteneciente al jugador
+						} while ( remColumna < 0 || remColumna > 2 );
+					} while ( remFila < 0 || remFila > 2 );
 				} while ( tab[ remFila ][ remColumna ] != player );
 				
 				tab[ remFila ][ remColumna ] = 0;
@@ -100,6 +163,6 @@ public class Tresenraya
 				
 				PUT_FICHA( player, tab );
 
-			} while ( CHECK_WINS() == false );
+			} while ( CHECK_WINS( player, tab ) == false );
 		}
 }
